39d107b2e766df4b0559d1320bc40c7178028e73
Mile Ubavin <mile.ubavin@edeja.com>
2024-05-10 13:01:04 +0200

    footer link test

---
+++ cypress/e2e/Datapart/dp footer links_PROD.js
@@ -0,0 +1,109 @@
+// describe('Invalid Login to DP', () => {
+//   it('Verify footer links', () => {
+//     //Get url from datapart.json / fixture folder
+//     cy.fixture('datapart.json').as('datapart');
+//     cy.get('@datapart').then((datapartJson) => {
+//       cy.visit(datapartJson.baseUrl); //Taken from base url
+//       cy.url().should('include', datapartJson.baseUrl); //Validating url on the login page
+//     });
+//     //Get tolal number of footer links, and validate total number (6 elements)
+//     let footerTitle = [];
+//     cy.get('.footer__wrapper > ul > li > a')
+//       .its('length')
+//       .should('eq', 6)
+//       .then((length) => {
+//         // length contains the total number of <a> elements
+//         cy.log('Total number of <a> elements:', length);
+//         cy.get('.footer__wrapper > ul > li > a')
+//           .each(($el, index, $list) => {
+//             footerTitle[index] = $el.text();
+//             cy.log('Footer title', footerTitle[index]);
+//           })
+//           //Validating footer titles
+//           .then(() => {
+//             for (let index = 0; index < length; index++) {
+//               cy.get('.footer__wrapper > ul > li > a')
+//                 .eq(index)
+//                 .invoke('text')
+//                 .as('footer__title');
+//               cy.get('@datapart').then((datapartJson) => {
+
+//                 cy.get('@footer__title').should(
+//                   'include',
+//                   datapartJson.footerTitle[index]
+//                 ); //Validate footerTitle
+//               });
+//             }
+//           });
+//       });
+//   }); //end it
+// });
+
+describe('Invalid Login to DP', () => {
+  it('Verify footer links', () => {
+    // Get url from datapart.json / fixture folder
+    cy.fixture('datapart.json').then((datapartJson) => {
+      cy.visit(datapartJson.baseUrl); // Taken from base url
+      cy.url().should('include', datapartJson.baseUrl); // Validating url on the login page
+
+      // Get footer titles from datapart.json
+      const footerTitles = [
+        datapartJson.footerTitle[0],
+        datapartJson.footerTitle[1],
+        datapartJson.footerTitle[2],
+        datapartJson.footerTitle[3],
+        datapartJson.footerTitle[4],
+        datapartJson.footerTitle[5],
+      ];
+
+      // Validate total number of footer links
+      cy.get('.footer__wrapper > ul > li > a').its('length').should('eq', 6);
+
+      // Validate each footer title
+      cy.get('.footer__wrapper > ul > li > a').each(($el, index) => {
+        cy.wrap($el).invoke('text').should('equal', footerTitles[index]);
+      });
+
+      // Click on each footer link and go back to the original page after each click
+      function clickNextLink(index) {
+        // Skip index 2
+        if (index === 2) {
+          index++; // Move to the next index
+        }
+
+        if (index >= footerTitles.length) {
+          return; // Exit when all links are clicked
+        }
+
+        cy.get('.footer__wrapper > ul > li > a')
+          .eq(index)
+          .invoke('removeAttr', 'target')
+          .click();
+        cy.wait(1000); // Adjust this wait time as needed
+
+        cy.url().then((url) => {
+          const footerLinks = [
+            datapartJson.footerLinks[0],
+            datapartJson.footerLinks[1],
+            datapartJson.footerLinks[2],
+            datapartJson.footerLinks[3],
+            datapartJson.footerLinks[4],
+            datapartJson.footerLinks[5],
+          ];
+          cy.log('URL after clicking on link:', url);
+          cy.wrap(url).should(
+            'eq',
+            'https://www.datapart-factoring.de/' + footerLinks[index]
+          ); // Assert the URL after clicking
+        });
+
+        cy.go('back').then(() => {
+          clickNextLink(index + 1); // Click the next link recursively
+        });
+      }
+
+      clickNextLink(0); // Start clicking from the first link
+    });
+    cy.log('Test completed successfully.');
+  });
+});
